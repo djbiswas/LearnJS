73f28fc8513f7d785bdd8cbed9e2ea00c7906445
='Dipjyoti <='djbiswasbd@gmail.com>
2019-12-18 18:29:54 +0600

    L16 - L18

---
+++ l16.js
@@ -0,0 +1,28 @@
+{
+	username: "andrei",
+	password: "supersecret"
+}
+
+// 2. Create an array which contains the object you have made above and name the arry "database"
+var database = [
+	{
+		username: "andrei",
+		password: "supersecret"
+	}
+];
+
+// 3. Create an array called "newsfeed" which contains 3 objects with properties "username" and "timeline".
+var newsfeed = [
+	{
+		username: "Bobby",
+		timeline: "So tired from all that learning!"
+	},
+	{
+		username: "Sally",
+		timeline: "Javascript is sooooo cool!"
+	},
+	{
+		username: "Mitch",
+		timeline: "Javascript is preeetyy cool!"
+	}
+];
